0921dc5f5f6a4d6cfbefeab0793216369e9b9422
Om Satapathy <omsatapathy05@gmail.com>
2025-02-01 02:17:42 +0530

    Added certain pre loaders at ViewCert and ViewImageGallery page

--- Frontend/src/pages/ViewImgGalleryPage.jsx
+++ Frontend/src/pages/ViewImgGalleryPage.jsx
@@ -14,7 +14,9 @@ const ViewImgGalleryPage = () => {
   const [albumsByYear, setAlbumsByYear] = useState([])
   const [selectedAlbum, setSelectedAlbum] = useState(null);
   const [albumImages, setAlbumImages] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [yearLoading, setYearLoading] = useState(true);
+  const [albumLoading, setAlbumLoading] = useState(true);
+  const [imageLoading, setImageLoading] = useState(true);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
   const [fullScreenImage, setFullScreenImage] = useState(null);
@@ -35,7 +37,7 @@ const ViewImgGalleryPage = () => {
       } catch (err) {
         setError("Failed to load years.");
       } finally {
-        setLoading(false);
+        setYearLoading(false);
       }
     };
     fetchYears();
@@ -48,7 +50,7 @@ const ViewImgGalleryPage = () => {
         setAlbumsByYear([]);
         return;
       }
-      setLoading(true);
+      setAlbumLoading(true);
       try {
         const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/albumsByYear/${selectedYear}`);
         setAlbumsByYear(response.data.albums || []);
@@ -57,7 +59,7 @@ const ViewImgGalleryPage = () => {
       } catch (err) {
         setError("Failed to load albums for selected year.");
       } finally {
-        setLoading(false);
+        setAlbumLoading(false);
       }
     };
     fetchAlbumsByYear();
@@ -68,7 +70,7 @@ const ViewImgGalleryPage = () => {
     const fetchAlbumImages = async () => {
       if (!selectedAlbum) return;
 
-      setLoading(true);
+      setImageLoading(true);
       setError(null); // Clear previous errors
 
       try {
@@ -91,7 +93,7 @@ const ViewImgGalleryPage = () => {
         setError(err.response?.data?.message || "Failed to load album images.");
         setAlbumImages(null);
       } finally {
-        setLoading(false);
+        setImageLoading(false);
       }
     };
     fetchAlbumImages();
@@ -131,8 +133,6 @@ const ViewImgGalleryPage = () => {
           setSuccess(null);
           return setError("Something went wrong while deleting the album");
         }
-
-        console.log(response.data);
         setLoading(false);
         setSuccess(response.data.message);
         setError(null);
@@ -182,6 +182,14 @@ const ViewImgGalleryPage = () => {
     setIsEditModalOpened(false);
   }
 
+  if (yearLoading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <svg height={250} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="none" stroke-opacity="1" stroke="#000000" stroke-width=".5" cx="100" cy="100" r="0"><animate attributeName="r" calcMode="spline" dur="2" values="1;80" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-width" calcMode="spline" dur="2" values="0;25" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" calcMode="spline" dur="2" values="1;0" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate></circle></svg>
+      </div>
+    )
+  }
+
   return (
     <div className="p-6 max-w-7xl mx-auto">
 
@@ -203,16 +211,16 @@ const ViewImgGalleryPage = () => {
               <button
                 onClick={closeDeleteModal}
                 className="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2 hover:bg-gray-400 transition-colors duration-300"
-                disabled={loading}
+                disabled={toBeDeletedItem.albumId && toBeDeletedItem.imageId ? imageLoading : albumLoading}
               >
                 Cancel
               </button>
               <button
                 onClick={handleDelete}
                 className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition-colors duration-300"
-                disabled={loading}
+                disabled={toBeDeletedItem.albumId && toBeDeletedItem.imageId ? imageLoading : albumLoading}
               >
-                {loading ? "Deleting..." : "Delete"}
+                {toBeDeletedItem.albumId && toBeDeletedItem.imageId ? imageLoading : albumLoading ? "Deleting..." : "Delete"}
               </button>
             </div>
           </div>
@@ -255,9 +263,9 @@ const ViewImgGalleryPage = () => {
               <button
                 type="submit"
                 className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700 transition-colors duration-300"
-                disabled={loading}
+                disabled={albumLoading}
               >
-                {loading ? 'Updating...' : 'Update Syllabus'}
+                {albumLoading ? 'Updating...' : 'Update Album'}
               </button>
             </form>
           </div>
@@ -277,7 +285,7 @@ const ViewImgGalleryPage = () => {
           value={selectedYear}
           onChange={(e) => setSelectedYear(e.target.value)}
           className="w-full p-2 border rounded"
-          disabled={loading}
+          disabled={yearLoading}
         >
           <option value="">Select a Year</option>
           {availableYears.map((year) => (
@@ -291,7 +299,11 @@ const ViewImgGalleryPage = () => {
         <div className="mb-6">
           <h2 className="text-xl font-semibold mb-2">Select Album</h2>
           <div className="flex flex-wrap gap-2 w-full">
-            {
+            {albumLoading ?
+              <div className="w-full h-full flex justify-center items-center">
+                <svg height={150} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="none" stroke-opacity="1" stroke="#000000" stroke-width=".5" cx="100" cy="100" r="0"><animate attributeName="r" calcMode="spline" dur="2" values="1;80" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-width" calcMode="spline" dur="2" values="0;25" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" calcMode="spline" dur="2" values="1;0" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate></circle></svg>
+              </div>
+              :
               albumsByYear.length === 0 ?
                 <div className="flex flex-col justify-center items-center text-xl h-full gap-y-4 w-full">
                   <div className="w-6 h-6">
@@ -336,96 +348,102 @@ const ViewImgGalleryPage = () => {
       )}
 
       {/* Images Display - Only show if album is selected */}
-      {loading && <div className="text-center">Loading...</div>}
+      {(!yearLoading && !albumLoading && selectedAlbum && imageLoading) ?
+        <div className="w-full h-full flex justify-center items-center">
+          <svg height={150} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="none" stroke-opacity="1" stroke="#000000" stroke-width=".5" cx="100" cy="100" r="0"><animate attributeName="r" calcMode="spline" dur="2" values="1;80" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-width" calcMode="spline" dur="2" values="0;25" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate><animate attributeName="stroke-opacity" calcMode="spline" dur="2" values="1;0" keyTimes="0;1" keySplines="0 .2 .5 1" repeatCount="indefinite"></animate></circle></svg>
+        </div>
+        :
 
-      {albumImages && (
-        <div>
-          <div className="flex justify-between items-center border-t-2 border-black pt-10">
-            <h2 className="text-2xl font-semibold mb-4"> Album : {albumImages.name}</h2>
-            <div className="flex flex-row justify-center items-center gap-x-4">
-              <button
-                className='bg-green-600 hover:bg-green-700 text-white flex justify-center items-center px-4 py-2 rounded-lg transition-colors duration-200'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  openEditModal(albumImages);
-                }}
-              >
-                <PencilIcon className="h-4 w-4 mr-1" />
-                Edit this album
-              </button>
+        albumImages && (
+          <div>
+            <div className="flex justify-between items-center border-t-2 border-black pt-10">
+              <h2 className="text-2xl font-semibold mb-4"> Album : {albumImages.name}</h2>
+              <div className="flex flex-row justify-center items-center gap-x-4">
+                <button
+                  className='bg-green-600 hover:bg-green-700 text-white flex justify-center items-center px-4 py-2 rounded-lg transition-colors duration-200'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    openEditModal(albumImages);
+                  }}
+                >
+                  <PencilIcon className="h-4 w-4 mr-1" />
+                  Edit this album
+                </button>
 
-              <button
-                className='bg-red-600 hover:bg-red-700 text-white flex justify-center items-center px-4 py-2 rounded-lg transition-colors duration-200'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  openDeleteModal(albumImages._id, null);
-                }}
-              >
-                <TrashIcon className="h-4 w-4 mr-1" />
-                Delete this album
-              </button>
+                <button
+                  className='bg-red-600 hover:bg-red-700 text-white flex justify-center items-center px-4 py-2 rounded-lg transition-colors duration-200'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    openDeleteModal(albumImages._id, null);
+                  }}
+                >
+                  <TrashIcon className="h-4 w-4 mr-1" />
+                  Delete this album
+                </button>
+              </div>
             </div>
-          </div>
-          <p className="text-gray-800 mb-6">Description: {albumImages.description}</p>
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
-            {
-              albumImages.images.length === 0 ?
-                <div className="flex flex-col justify-center items-center text-xl h-full gap-y-4 w-full md:col-span-3 lg:col-span-4 ">
-                  <div className="w-6 h-6">
-                    <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                      <g id="SVGRepo_iconCarrier">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8.4449 0.608765C8.0183 -0.107015 6.9817 -0.107015 6.55509 0.608766L0.161178 11.3368C-0.275824 12.07 0.252503 13 1.10608 13H13.8939C14.7475 13 15.2758 12.07 14.8388 11.3368L8.4449 0.608765ZM7.4141 1.12073C7.45288 1.05566 7.54712 1.05566 7.5859 1.12073L13.9798 11.8488C14.0196 11.9154 13.9715 12 13.8939 12H1.10608C1.02849 12 0.980454 11.9154 1.02018 11.8488L7.4141 1.12073ZM6.8269 4.48611C6.81221 4.10423 7.11783 3.78663 7.5 3.78663C7.88217 3.78663 8.18778 4.10423 8.1731 4.48612L8.01921 8.48701C8.00848 8.766 7.7792 8.98664 7.5 8.98664C7.2208 8.98664 6.99151 8.766 6.98078 8.48701L6.8269 4.48611ZM8.24989 10.476C8.24989 10.8902 7.9141 11.226 7.49989 11.226C7.08567 11.226 6.74989 10.8902 6.74989 10.476C6.74989 10.0618 7.08567 9.72599 7.49989 9.72599C7.9141 9.72599 8.24989 10.0618 8.24989 10.476Z" fill="#000000">
-                        </path>
-                      </g>
-                    </svg>
-                  </div>
-                  No images added
-                  <button
-                    className="py-2 px-4 bg-green-600 hover:bg-green-700 flex justify-center items-center text-white rounded-xl transition-colors duration-200"
-                    onClick={() => { navigate('/admin-add-gallery') }}
-                  >
-                    <div className="w-4 h-4 mr-4">
-                      <svg fill="#ffffff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 45.402 45.402" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141 c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27 c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435 c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"></path> </g> </g></svg>
+            <p className="text-gray-800 mb-6">Description: {albumImages.description}</p>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
+              {
+                albumImages.images.length === 0 ?
+                  <div className="flex flex-col justify-center items-center text-xl h-full gap-y-4 w-full md:col-span-3 lg:col-span-4 ">
+                    <div className="w-6 h-6">
+                      <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M8.4449 0.608765C8.0183 -0.107015 6.9817 -0.107015 6.55509 0.608766L0.161178 11.3368C-0.275824 12.07 0.252503 13 1.10608 13H13.8939C14.7475 13 15.2758 12.07 14.8388 11.3368L8.4449 0.608765ZM7.4141 1.12073C7.45288 1.05566 7.54712 1.05566 7.5859 1.12073L13.9798 11.8488C14.0196 11.9154 13.9715 12 13.8939 12H1.10608C1.02849 12 0.980454 11.9154 1.02018 11.8488L7.4141 1.12073ZM6.8269 4.48611C6.81221 4.10423 7.11783 3.78663 7.5 3.78663C7.88217 3.78663 8.18778 4.10423 8.1731 4.48612L8.01921 8.48701C8.00848 8.766 7.7792 8.98664 7.5 8.98664C7.2208 8.98664 6.99151 8.766 6.98078 8.48701L6.8269 4.48611ZM8.24989 10.476C8.24989 10.8902 7.9141 11.226 7.49989 11.226C7.08567 11.226 6.74989 10.8902 6.74989 10.476C6.74989 10.0618 7.08567 9.72599 7.49989 9.72599C7.9141 9.72599 8.24989 10.0618 8.24989 10.476Z" fill="#000000">
+                          </path>
+                        </g>
+                      </svg>
                     </div>
-                    <p>
-                      Upload a new image
-                    </p>
-                  </button>
-                </div>
-                :
-                albumImages.images.map((image) => (
-                  <div key={image._id} className="relative group">
-                    <img
-                      src={image.image}
-                      alt=""
-                      className="w-full h-48 object-cover rounded cursor-pointer"
-                      onClick={() => setFullScreenImage(image.image)}
-                    />
+                    No images added
                     <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        openDeleteModal(albumImages._id, image._id);
-                      }}
-                      className="absolute bottom-3 right-2 bg-red-500 hover:bg-red-600 text-white rounded-l opacity-0 group-hover:opacity-100 transition-all duration-200 flex items-center justify-center "
-                      disabled={deleting}
-                      title="Delete image"
+                      className="py-2 px-4 bg-green-600 hover:bg-green-700 flex justify-center items-center text-white rounded-xl transition-colors duration-200"
+                      onClick={() => { navigate('/admin-add-gallery') }}
                     >
-                      {deleting ? (
-                        <span className="animate-spin">...</span>
-                      ) : (
-                        <div className="flex justify-center items-center gap-x-4 px-4 py-2">
-                          <TrashIcon className="h-6 w-6 " />
-                          <p>Delete Image</p>
-                        </div>
-                      )}
+                      <div className="w-4 h-4 mr-4">
+                        <svg fill="#ffffff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 45.402 45.402" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141 c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27 c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435 c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"></path> </g> </g></svg>
+                      </div>
+                      <p>
+                        Upload a new image
+                      </p>
                     </button>
                   </div>
-                ))}
+                  :
+                  albumImages.images.map((image) => (
+                    <div key={image._id} className="relative group">
+                      <img
+                        src={image.image}
+                        alt=""
+                        className="w-full h-48 object-cover rounded cursor-pointer"
+                        onClick={() => setFullScreenImage(image.image)}
+                      />
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          openDeleteModal(albumImages._id, image._id);
+                        }}
+                        className="absolute bottom-3 right-2 bg-red-500 hover:bg-red-600 text-white rounded-l opacity-0 group-hover:opacity-100 transition-all duration-200 flex items-center justify-center "
+                        disabled={deleting}
+                        title="Delete image"
+                      >
+                        {deleting ? (
+                          <span className="animate-spin">...</span>
+                        ) : (
+                          <div className="flex justify-center items-center gap-x-4 px-4 py-2">
+                            <TrashIcon className="h-6 w-6 " />
+                            <p>Delete Image</p>
+                          </div>
+                        )}
+                      </button>
+                    </div>
+                  ))}
+            </div>
           </div>
-        </div>
-      )}
+        )
+      }
+
 
       {/* Fullscreen Modal */}
       {fullScreenImage && (
